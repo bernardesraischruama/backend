6875676850b34b56baadfc02d595f87899352d35
bernardesraischruama <bernardesraichruama@gmail.com>
2025-02-10 11:36:09 -0300

    aula 2

--- alunos/views.py
+++ alunos/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from alunos.models import Estado
+from alunos.serializers import EstadoSerializer
 
-# Create your views here.
+class EstadoViewSet(ModelViewSet):
+    queryset = Estado.objects.all()
+    serializer_class = EstadoSerializer
